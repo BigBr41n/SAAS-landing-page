3d4f411a6eea91d6b723410a7ec89289fcda1522
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-05 19:04:01 +0200

    Banner

---
+++ src/components/Banner.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div
+	class="mx-auto bg-[linear-gradient(to_right,rgb(252,_214,_255_,_.7),rgb(41,_216,_255_,_.7),rgb(255,_253,_128_,_.7),rgb(248,_154,_191_,_.7),rgb(252,_214,_255_,_.7))] py-3 text-center"
+>
+	<div class="container mx-auto">
+		<p class="">
+			<span class="hidden sm:inline">introducing a completely redesigned interface - </span>
+			<a href="#noo" class="font-medium underline"> Explore the demo</a>
+		</p>
+	</div>
+</div>
