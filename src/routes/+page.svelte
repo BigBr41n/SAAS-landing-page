c374aa73bb9c04159c3acb6f75e6d91abf77af9a
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-06 21:50:59 +0200

    Features section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import Features from '../components/Features.svelte';
 	import Hero from '../components/Hero.svelte';
 	import LogoTicker from '../components/LogoTicker.svelte';
 </script>
 
 <Hero />
 <LogoTicker />
+<Features />
