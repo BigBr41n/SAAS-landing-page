9f577f3e2082c36b33297f4d7b83ad5da60c6f0d
R4him <148691250+BigBr41n@users.noreply.github.com>
2024-08-06 22:06:36 +0200

    product showcase section

---
+++ src/components/ProductShowCase.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="bg-black bg-gradient-to-b from-black to-[#5D2AC8] py-[72px] text-white">
+	<div class="container">
+		<h2 class="text-center text-5xl font-bold tracking-tighter sm:text-6xl">Intuitive interface</h2>
+		<div class="mx-auto max-w-xl">
+			<p class="mt-5 text-center text-lg text-white/70">
+				Celebrate the joy of accomplishment with an app designed to track progress , motivation your
+				efforts, and celebrate your success.
+			</p>
+		</div>
+		<img class="mx-auto mt-14" src="/assets/images/app-screen.png" alt="the product screenshot" />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import Features from '../components/Features.svelte';
 	import Hero from '../components/Hero.svelte';
 	import LogoTicker from '../components/LogoTicker.svelte';
+	import ProductShowCase from '../components/ProductShowCase.svelte';
 </script>
 
 <Hero />
 <LogoTicker />
 <Features />
+<ProductShowCase />
